ba5e1002aecbe8d33425c0f5db823d1de3c79d43
Felix Manuel <luxotus@gmail.com>
2018-09-15 11:56:56 -0400

    spell checker fix

--- curve-fit-example.py
+++ curve-fit-example.py
@@ -3,8 +3,8 @@ import scipy.optimize as optimization
 
 # Generate artificial data = straight line with a=0 and b=1
 # plus some noise.
-xdata = numpy.array([0.0,1.0,2.0,3.0,4.0,5.0])
-ydata = numpy.array([0.1,0.9,2.2,2.8,3.9,5.1])
+xData = numpy.array([0.0,1.0,2.0,3.0,4.0,5.0])
+yData = numpy.array([0.1,0.9,2.2,2.8,3.9,5.1])
 
 # Initial guess.
 x0    = numpy.array([0.0, 0.0, 0.0])
@@ -15,4 +15,4 @@ sigma = numpy.array([1.0,1.0,1.0,1.0,1.0,1.0])
 def model(x, a, b, c):
   return a + b*x + c*x*x
 
-print(optimization.curve_fit(model, xdata, ydata, x0, sigma))
+print(optimization.curve_fit(model, xData, yData, x0, sigma))
